60e33f497489902bd844181f40333f7f9dce19dd
sakhasakhar <jenderwd@gmail.com>
2021-11-27 19:44:03 +0300

    random colors

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import sys
-from PyQt5 import uic
+from PyQt5 import QtCore, QtWidgets
 from random import randrange
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QInputDialog
+from PyQt5.QtWidgets import QWidget, QApplication
 
 
-class Example(QWidget):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(500, 500)
+        Form.setStyleSheet("background-color: rgb(43, 85, 149)")
+        self.drawBtn = QtWidgets.QPushButton(Form)
+        self.drawBtn.setGeometry(QtCore.QRect(190, 440, 131, 31))
+        self.drawBtn.setObjectName("drawBtn")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.drawBtn.setText(_translate("Form", "Нарисовать"))
+
+
+class Example(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.setWindowTitle('Жолтые окружности....`')
         self.do_paint = False
         self.drawBtn.clicked.connect(self.paint)
@@ -25,7 +43,7 @@ class Example(QWidget):
         self.repaint()
 
     def draw_flag(self, qp):
-        qp.setPen(QColor(255, 255, 0))
+        qp.setPen(QColor(randrange(255), randrange(255), randrange(255)))
         r = randrange(20, 250)
         qp.drawEllipse(250 - r, 250 - r, r * 2, r * 2)
 
